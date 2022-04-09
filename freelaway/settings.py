731c01b5f902410d6802122818080bbf5fdb6ba3
DKsHs <nicolas.contiero198@gmail.com>
2022-04-09 17:08:40 -0300

    Adicionando o AWS

--- freelaway/settings.py
+++ freelaway/settings.py
@@ -145,4 +145,14 @@ MESSAGE_TAGS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# AWS 
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 django_on_heroku.settings(locals())
